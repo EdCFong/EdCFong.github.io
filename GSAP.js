0ffa92eae86d6aa7f64f2fd4b86878058d60e5da
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2021-06-28 20:48:57 -0400

    Update June 28, 2021

--- GSAP.js
+++ GSAP.js
@@ -1,4 +1,13 @@
 CustomEase.create("customPower0", "M0,0 C0.083,0.294 0.122,0.82 0.388,1.01 0.477,1.073 0.497,0.989 0.684,0.968 0.742,0.961 0.803,0.988 0.86,1 0.887,1.005 0.96,1.014 1,1 ")
+gsap.registerPlugin(ScrollTrigger);
+
+
+
+//   ###########################################################################################
+//   Certifications
+
+//   ###########################################################################################   
+
 var animationTime = 0.8;
 var dotColor = "#000000";
 // Dot 0 *************************************************************************************** 
@@ -32,3 +41,24 @@ document.querySelector("#dot1").onclick = () => {
 
 
 
+
+
+//   ###########################################################################################
+//   Head
+
+//   ###########################################################################################   
+
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+tl.to("#presentationMessage", { yPercent: 90, xPercent: -40  })
+    .to("#presentationMessage h3", { opacity: 0, duration: 0.5 }, 0.3)
+    //.to("#presentationMessage", { opacity: 0 }, 2)
+   // .to("#head nav h1", {opacity:1}, 1.5)
+
+
+ScrollTrigger.create({
+    animation: tl,
+    trigger: "#presentationMessage h1",
+    start: "top top",
+    end: "top top",
+    scrub: 0.5
+});
